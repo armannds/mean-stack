e717484b2c4b7ab06aa7470af21a821cd11899f0
Armann David Sigurdsson <armannds@gmail.com>
2018-08-10 10:11:34 +0200

    Refactor backend routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-const Post = require('./models/post')
+const postRoutes = require('./routes/posts')
 
 const app = express()
 
@@ -23,60 +23,6 @@ app.use((req, res, next) => {
   next()
 })
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  })
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: result._id
-    })
-  })
-})
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-  Post.updateOne({_id: req.params.id}, post)
-    .then((result) => {
-      res.status(200).json({message: 'Update successfull'})
-    })
-})
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then(docs => {
-      res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: docs
-      })
-    })
-})
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById({_id: req.params.id})
-    .then(post => {
-      if (post) {
-        res.status(200).json({message: 'Post found', post: post})
-      } else {
-        res.status(404).json({message: 'Post not found!'})
-      }
-    })
-})
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({
-    _id: req.params.id
-  }).then(() => {
-    res.status(200).json({
-      message: 'Post deleted'
-    })
-  })
-})
+app.use('/api/posts', postRoutes)
 
 module.exports = app
